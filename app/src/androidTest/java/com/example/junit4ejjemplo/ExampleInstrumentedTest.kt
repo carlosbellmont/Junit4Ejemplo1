3615dd55fbe6ed5013e2f1d8c5ca28e5e59cc6bb
cbellmont <carlos.bellmont@gmail.com>
2022-01-15 15:51:08 +0100

    Updating

--- app/src/androidTest/java/com/example/junit4ejjemplo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/junit4ejjemplo/ExampleInstrumentedTest.kt
@@ -1,27 +1,52 @@
 package com.example.junit4ejjemplo
 
 import android.content.Context
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.AfterClass
 import org.junit.Assert.assertEquals
 import org.junit.BeforeClass
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
+
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
 
     companion object {
 
         private lateinit var applicationName : String
         private lateinit var appContext: Context
+        private var startingTime = 0L
 
         @BeforeClass
         @JvmStatic
         fun initVariables() {
             appContext = InstrumentationRegistry.getInstrumentation().targetContext
             applicationName = appContext.resources.getString(R.string.app_name)
+            startingTime = System.currentTimeMillis()
+
+        }
+
+
+        @AfterClass
+        @JvmStatic
+        fun showTotalTime() {
+            println("The unit test were executed in ${System.currentTimeMillis() - startingTime} ")
         }
     }
 
@@ -47,6 +72,18 @@ class ExampleInstrumentedTest {
     @Test
     fun checkApplicationNameLengthIs14() {
         assert(applicationName.length == 14)
+    }
+
+
+    @Test
+    fun checkValidEmailAndPass() {
+        //onView(withId(R.id.etEmail)).perform(click())
+        var firstActivity: IntentsTestRule<MainActivity> = IntentsTestRule(MainActivity::class.java)
+        firstActivity.launchActivity(Intent())
+
+        onView(withId(R.id.etEmail)).perform(typeText("my text"), closeSoftKeyboard())
+        onView(withId(R.id.bLogIn)).perform(click())
+        onView(withId(com.google.android.material.R.id.snackbar_text)).check(matches(withText(R.string.snack_logged)))
     }
 
 }
--- app/src/test/java/com/example/junit4ejjemplo/ExampleUnitTest.kt
+++ app/src/test/java/com/example/junit4ejjemplo/ExampleUnitTest.kt
@@ -1,7 +1,9 @@
 package com.example.junit4ejjemplo
 
 import com.example.junit4ejjemplo.ui.main.MainViewModel
-import org.junit.Assert.*
+import org.junit.AfterClass
+import org.junit.Assert.assertFalse
+import org.junit.BeforeClass
 import org.junit.Test
 
 
@@ -9,74 +11,74 @@ class ExampleUnitTest {
 
     private var viewModel = MainViewModel()
 
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+    companion object {
+        private var startingTime = 0L
+
+        @BeforeClass
+        @JvmStatic
+        fun checkInitialTime() {
+            startingTime = System.currentTimeMillis()
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun showTotalTime() {
+            println("The unit test were executed in ${System.currentTimeMillis() - startingTime} ")
+        }
+
     }
 
     @Test
-    fun isEmailValidWithValidEmail1(){
-        assert(viewModel.isEmailValid("testerio@testeador.test"))
+    fun `testing isValidEmail with correct values`(){
+        val correctValues = arrayOf(
+            "testerio@testeador.test",
+            "Testerio1@testeador.test",
+            "TESTERIO@testeador.test",
+            "testerio.tester@testeador.test"
+        )
+        correctValues.forEach { email ->
+            assert(viewModel.isEmailValid(email))
+        }
     }
 
     @Test
-    fun isEmailValidInvalidNoDot(){
-        assertFalse(viewModel.isEmailValid("testerio@testeador,test"))
+    fun `testing isValidEmail with wrong values`(){
+        val wrongValues = arrayListOf(
+            "testerio@testeador,test", // no dot
+            "testeriotesteador.test", // no @
+            "testerio@tester@testeador.test" // Double @@
+        )
+        wrongValues.forEach { email ->
+            assertFalse(viewModel.isEmailValid(email))
+        }
+    }
+
+
+    @Test
+    fun `testing isValidPassword with correct values`(){
+        val correctValues = arrayOf(
+            "0123456789aZ!",
+        )
+        correctValues.forEach { email ->
+            assert(viewModel.isPasswordValid(email))
+        }
     }
 
     @Test
-    fun isEmailValidWithNoAt(){
-        assertFalse(viewModel.isEmailValid("testeriotesteador.test"))
-    }
-
-    @Test
-    fun isEmailValidWithDoubleDot(){
-        assertTrue(viewModel.isEmailValid("testerio.tester@testeador.test"))
-    }
-
-    @Test
-    fun isEmailValidWithDoubleAt(){
-        assertFalse(viewModel.isEmailValid("testerio@tester@testeador.test"))
-    }
-
-    @Test
-    fun isPasswordValidWithValidPassword(){
-        assert(viewModel.isPasswordValid("0123456789aZ!"))
-    }
-
-    @Test
-    fun isPasswordValidTooLarge(){
-        assertFalse(viewModel.isPasswordValid("01234567890123456789aZ!"))
-    }
-
-    @Test
-    fun isPasswordValidTooShort(){
-        assertFalse(viewModel.isPasswordValid("0aZ!"))
-    }
-
-    @Test
-    fun isPasswordValidEmpty(){
-        assertFalse(viewModel.isPasswordValid(""))
-    }
-
-    @Test
-    fun isPasswordValidNoLoweCase(){
-        assertFalse(viewModel.isPasswordValid("0123456789Z!"))
-    }
-
-    @Test
-    fun isPasswordValidNoUpperCase(){
-        assertFalse(viewModel.isPasswordValid("0123456789a!"))
-    }
-
-    @Test
-    fun isPasswordValidNoSymbol(){
-        assertFalse(viewModel.isPasswordValid("0123456789aZ"))
-    }
-
-    @Test
-    fun isPasswordValidNoNumber(){
-        assertFalse(viewModel.isPasswordValid("aZ!aZ!aZ!"))
+    fun `testing isValidPassword with wrong values`(){
+        val wrongValues = arrayListOf(
+            "01234567890123456789aZ!", // too large
+            "0aZ!", // too short
+            "", // Empty
+            "0123456789Z!", // no lower case
+            "0123456789a!", // no upper case
+            "0123456789aZ", // no symbol
+            "aZ!aZ!aZ!", // no number
+        )
+        wrongValues.forEach { email ->
+            assertFalse(viewModel.isEmailValid(email))
+        }
     }
 
 }
